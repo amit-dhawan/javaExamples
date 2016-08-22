e01a92a32048990ec1452058b47253d4b7e1c37e
amit1985 <amit.dhawan085@gmail.com>
2016-08-22 15:47:46 +0530

    Calc method commit

---
+++ LearnJavaExamples/src/learn/basicExamples/CalculatorJava.java
@@ -0,0 +1,31 @@
+package learn.basicExamples;
+
+import learn.basicfunctions.CalcFunctions;
+
+public class CalculatorJava {
+
+	static float num1 = 5;
+	static float num2 = 2;
+
+	static float sum;
+	static float subtraction;
+	static float multiplication;
+	static float division;
+
+	static CalcFunctions c;
+
+	public static void main(String[] args) {
+
+		c = new CalcFunctions();
+
+		System.out.println("num1: " + num1 + " and num2: " + num2);
+		System.out.println("Sum of num1, num2 is : " + c.add(num1, num2));
+		System.out.println("Subtraction of num1, num2 is : "
+				+ c.subtract(num1, num2));
+		System.out.println("Multiplication of num1, num2 is : "
+				+ c.multiply(num1, num2));
+		System.out.println("Division of num1, num2 is : " + c.divide(num1, num2));
+
+	}
+
+}
---
+++ LearnJavaExamples/src/learn/basicfunctions/CalcFunctions.java
@@ -0,0 +1,28 @@
+package learn.basicfunctions;
+
+public class CalcFunctions {
+
+	
+	public float add(float num1, float num2){
+		
+		return num1+num2;
+	}
+	
+public float subtract(float num1, float num2){
+		
+		return num1-num2;
+	}
+
+public float multiply(float num1, float num2){
+	
+	return num1*num2;
+}
+
+public float divide(float num1, float num2){
+	
+	return num1/num2;
+}
+	
+	
+	
+}
